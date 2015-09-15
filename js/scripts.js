b74e67773a86fec567d2490085aac0b830f35cf6
Sam Martinez & Kyle Pratuch <student@epicodus.com>
2015-09-15 14:41:40 -0700

    add specs: pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 //JavaScript goes here
+
+function Account(userName, balance) {
+    this.userName = userName;
+    this.balance = balance;
+}
+
+Account.prototype.deposit = function(depositAmount) {
+     parseInt(this.balance += depositAmount);
+    return this.balance;
+}
+
+Account.prototype.withdraw = function(withdrawAmount) {
+     parseInt(this.balance -= withdrawAmount);
+     return this.balance;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,27 @@
 //JavaScript specs go here.
+describe('Account', function() {
+
+    it("creates a new user with a name and initial deposit amount", function() {
+        var testAccount = new Account("Lucile Bluth", 100);
+        expect(testAccount.userName).to.equal("Lucile Bluth");
+        expect(testAccount.balance).to.equal(100);
+    });
+
+    it("checks the balance of the account", function() {
+        var testAccount = new Account("Lucile Bluth", 100);
+        expect(testAccount.balance).to.equal(100);
+    });
+
+    it("checks the account balance after a deposit is made", function() {
+        var testAccount = new Account("Lucile Bluth", 100);
+        var depositAmount = 150;
+        expect(testAccount.deposit(depositAmount)).to.equal(250);
+    });
+
+    it("checks the account balance after a withdrawal is made", function() {
+        var testAccount = new Account("Lucile Bluth", 100);
+        var withdrawAmount = 50;
+        expect(testAccount.withdraw(withdrawAmount)).to.equal(50);
+    });
+
+});
